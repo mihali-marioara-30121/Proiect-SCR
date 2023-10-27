e6cfbf0333a49a2285aed4435f4f11664b9b7088
mihali-marioara-30121 <marioaramihali2002@gmail.com>
2023-10-27 17:55:53 +0300

    project starts now!

---
+++ src/test/java/com/proiect/scd/proiectSCD/ProiectScdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.scd.proiectSCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectScdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
