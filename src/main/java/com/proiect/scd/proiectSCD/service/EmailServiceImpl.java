06b4c57909aba2a414cff5e8036b78a211d44075
mihali-marioara-30121 <marioaramihali2002@gmail.com>
2023-11-04 11:23:21 +0200

    imbunatatire pentru email

--- src/main/java/com/proiect/scd/proiectSCD/service/EmailServiceImpl.java
+++ src/main/java/com/proiect/scd/proiectSCD/service/EmailServiceImpl.java
@@ -20,22 +20,37 @@ public class EmailServiceImpl implements EmailService {
         MailjetRequest request;
         MailjetResponse response;
         client = new MailjetClient("370d3bafade5bef577969e9dbed8f180", "5b02dd0eb17c77d92d56853941d4521b");
+
+        Employee Lucian = new Employee();
+        Lucian.setEmail("lucian.miholca@gmail.com");
+        Lucian.setName("Lucian");
+
+        Employee Marioara = new Employee();
+        Marioara.setEmail("marioaramihali2002@gmail.com");
+        Marioara.setName("Marioara");
+
+        employeeList = List.of(Lucian, Marioara);
+
+        JSONArray recieversList = new JSONArray();
+        for (Employee employee: employeeList) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Email", employee.getEmail());
+            jsonObject.put("Name", employee.getName());
+
+            recieversList.put(jsonObject);
+        }
+
         request = new MailjetRequest(Emailv31.resource)
                 .property(Emailv31.MESSAGES, new JSONArray()
                         .put(new JSONObject()
                                 .put(Emailv31.Message.FROM, new JSONObject()
                                         .put("Email", "scdemailsender@gmail.com")
                                         .put("Name", "Mailjet Pilot"))
-                                .put(Emailv31.Message.TO, new JSONArray()
-                                        .put(new JSONObject()
-                                                .put("Email", "marioaramihali2002@gmail.com")
-                                                .put("Name", "passenger 1"))
-                                        .put(new JSONObject()
-                                                .put("Email", "lucian.miholca@gmail.com")
-                                                .put("Name", "passenger 2")))
+                                .put(Emailv31.Message.TO, recieversList)
                                 .put(Emailv31.Message.SUBJECT, "Your email flight plan!")
                                 .put(Emailv31.Message.TEXTPART, "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!")
                                 .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!")));
+
         response = client.post(request);
         System.out.println(response.getStatus());
         System.out.println(response.getData());
